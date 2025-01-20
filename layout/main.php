cf7b0f4fa0b55d68b45f73b3a1973357b64f4472
FernandoYZ <fernandozavala266@gmail.com>
2025-01-20 13:39:08 -0500

    feat(layout): agregar main.php en el layout como plantilla

---
+++ layout/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yamelin Peves Bakery</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+<body>
+    <?php include 'components/navbar.php'; ?>
+    <main class="container">
+        <?php include $pagePath; ?>
+    </main>
+    <?php include 'components/footer.php'; ?>
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/navbar.js"></script>
+</body>
+</html>
